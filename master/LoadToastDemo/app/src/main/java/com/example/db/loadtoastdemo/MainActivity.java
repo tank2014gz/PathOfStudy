25dc44c75eebccc93e32dc7106ff3fe843da75a0
KGDongBin <3025673709@qq.com>
2015-07-12 15:29:12 +0800

    demo

---
+++ master/LoadToastDemo/app/src/main/java/com/example/db/loadtoastdemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.db.loadtoastdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class MainActivity extends Activity {
+
+    // Example activity
+
+    int delay = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String text = "";
+        final LoadToast lt = new LoadToast(this).setText(text).setTranslationY(100).show();
+        //lt.success();
+        final ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+
+        View v = new View(this);
+        v.setBackgroundColor(Color.RED);
+        root.addView(v, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 400));
+        findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lt.show();
+            }
+        });
+        findViewById(R.id.error).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lt.error();
+            }
+        });
+        findViewById(R.id.success).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lt.success();
+            }
+        });
+        findViewById(R.id.refresh).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View v = new View(MainActivity.this);
+                v.setBackgroundColor(Color.rgb((int)(Math.random() * 255), (int)(Math.random() * 255), (int)(Math.random() * 255)));
+                root.addView(v, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 400));
+            }
+        });
+    }
+}
